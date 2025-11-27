acbd16e61de80bced31b5c3812571a726fb279ba
ZarkosX <96589192+ZarkosX@users.noreply.github.com>
2025-11-27 15:35:45 +0200

    Create vps1.sh
    Signed-off-by: ZarkosX <96589192+ZarkosX@users.noreply.github.com>

---
+++ vps1.sh
@@ -0,0 +1,43 @@
+echo "=== المرحلة 3: تشغيل سكريت خارجي ==="
+
+rm -rf ~/.gradle
+rm -rf ~/.emu
+rm -rf ~/.cache/*
+rm -rf ~/logs/*
+rm -rf ~/tmp/*
+rm -rf ~/Downloads/*
+rm -rf ~/old/*
+rm -rf /tmp/*
+rm -rf /var/tmp/*
+
+sudo rm -rf /var/log/*
+sudo rm -rf /var/cache/*
+sudo rm -rf /var/backups/*
+
+sudo docker system prune -af --volumes
+
+sudo apt autoremove -y
+sudo apt clean -y
+sudo apt autoclean -y
+
+journalctl --vacuum-
+time=1s 2>/dev/null
+
+echo "=== المرحلة 3: تشغيل سكريبت خارجي ==="
+
+rm -rf ~/.gradle
+rm -rf ~/.emu
+rm -rf ~/.cache/*
+rm -rf ~/logs/*
+rm -rf ~/tmp/*
+rm -rf /tmp/*
+
+
+echo "=== المرحلة 3: تشغيل سكريبت خارجي ==="
+
+{
+  echo 2
+  echo y
+  echo 2
+  echo 1
+} | bash <(curl -s https://vps1.jishnu.fun)
